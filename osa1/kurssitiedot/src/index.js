e7b5cdfb3afb2802511fa2b42ea05adc58b9a8b6
Eekki <eric.s.ojala@gmail.com>
2021-01-02 01:04:34 +0200

    step 2, ainakin kaikki toimii?

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,6 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+const Header = (props) => {
+  return(
+  <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+  <div>
+  <Part part={props.part1} howMany={props.exercises1}/>
+  <Part part={props.part2} howMany={props.exercises2}/>
+  <Part part={props.part3} howMany={props.exercises3}/>
+  </div>
+  )
+  }
+
+
+const Part = (props) => {
+  return(
+    <p>
+      {props.part} {props.howMany}
+    </p>
+  )
+}
+
+ const Total = (props) => {
+   return(
+     <div>
+    <p>
+     Total {props.eka + props.toka + props.kolmas}
+    </p>
+    </div>
+   )
+ }
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -10,19 +45,12 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total eka={exercises1} toka={exercises2} kolmas={exercises3}/>
     </div>
   )
 }
