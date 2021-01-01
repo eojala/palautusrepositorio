d13d7ab593b7d7ff5bbbb9c54c966b8eaef695ab
Eekki <eric.s.ojala@gmail.com>
2021-01-02 01:56:08 +0200

    step3 muutettu vakiot, koodi toimii taas

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -11,9 +11,9 @@ const Header = (props) => {
 const Content = (props) => {
   return(
   <div>
-  <Part part={props.part1} howMany={props.exercises1}/>
-  <Part part={props.part2} howMany={props.exercises2}/>
-  <Part part={props.part3} howMany={props.exercises3}/>
+  <Part name={props.parts[0].name} howMany={props.parts[0].exercises}/>
+  <Part name={props.parts[1].name} howMany={props.parts[1].exercises}/>
+  <Part name={props.parts[2].name} howMany={props.parts[2].exercises}/>
   </div>
   )
   }
@@ -21,36 +21,45 @@ const Content = (props) => {
 
 const Part = (props) => {
   return(
-    <p>
-      {props.part} {props.howMany}
+    <p>     
+     {props.name} {props.howMany}
     </p>
   )
 }
 
  const Total = (props) => {
+   console.log(props.numero[0].exercises)
    return(
      <div>
     <p>
-     Total {props.eka + props.toka + props.kolmas}
+     Total {props.numero[0].exercises + props.numero[1].exercises + props.numero[2].exercises}
     </p>
     </div>
    )
  }
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
+  let kurssit = [part1, part2, part3]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total eka={exercises1} toka={exercises2} kolmas={exercises3}/>
+      <Content parts={kurssit}/>
+      <Total numero={kurssit}/>
     </div>
   )
 }
