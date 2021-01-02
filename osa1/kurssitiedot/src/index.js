3428ee3dac86c5157dfb9c69acdf180d18db3938
Eekki <eric.s.ojala@gmail.com>
2021-01-02 02:01:11 +0200

    step4, homma rokkaa

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -28,7 +28,6 @@ const Part = (props) => {
 }
 
  const Total = (props) => {
-   console.log(props.numero[0].exercises)
    return(
      <div>
     <p>
@@ -40,26 +39,28 @@ const Part = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
-  let kurssit = [part1, part2, part3]
+
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={kurssit}/>
-      <Total numero={kurssit}/>
+      <Content parts={parts}/>
+      <Total numero={parts}/>
     </div>
   )
 }
